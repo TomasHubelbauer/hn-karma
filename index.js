000b244f66bd2acccd75840a242a579b3b17818b
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-05-10 17:46:56 +0200

    Use node-fetch and return instead of emailing

--- index.js
+++ index.js
@@ -1,79 +1,47 @@
-// TODO: `fetch`
-const https = require('https');
+const fetch = require('node-fetch');
 const fs = require('fs-extra');
 const path = require('path');
-const email = require('../self-email');
-const { eml, subject, sender, recipient } = require('../self-email');
 
-module.exports = async function () {
-  const user = process.argv[2] || process.env.HN_USER;
+module.exports = async function (/** @type {string} */ user) {
+  user = user || process.env.HN_USER;
   if (!user) {
     throw new Error('The `user` command line argument or `HN_USER` environment variable not provided.');
   }
 
-  const token = process.argv[3] || process.env.HN_TOKEN;
+  const response = await fetch(`https://news.ycombinator.com/user?id=${user}`);
+  const text = await response.text();
+  const regex = /<tr>\s*<td[^>]+>\s*karma:\s*<\/td>\s*<td>\s*(?<karma>\d+)\s*<\/td><\/tr>/;
+  const match = regex.exec(text);
+  if (!match) {
+    throw new Error(`The pattern ${regex.source} not found on https://news.ycombinator.com/user?id=${user}.`);
+  }
 
-  /** @type {https.RequestOptions} */
-  const options = token ?
-    {
-      protocol: 'https:',
-      host: 'news.ycombinator.com',
-      headers: {
-        Cookie: `user=${user}&${token}`,
-      }
-    }
-    : `https://news.ycombinator.com/user?id=${user}`;
+  const karmaHnFilePath = path.join(__dirname, 'karma.hn');
 
-  const userPageRegex = new RegExp(`<a id='me' href="user\\?id=${user}">${user}<\\/a>\\s+\\((?<karma>\\d+)\\)`, 'gm');
-  const homePageRegex = new RegExp(`<td valign="top">karma:<\\/td><td>\\s*(?<karma>\\d+)\\s*<\/td>`);
+  const karma = Number(match.groups.karma);
+  console.log(`Hacker News karma: ${karma}`);
 
-  const request = https.request(options, response => {
-    if (response.statusCode !== 200 || response.statusMessage !== 'OK') {
-      throw new Error(`Expected 200 OK but got ${response.statusCode} ${response.statusMessage}.`);
-    }
+  let oldKarma;
+  try {
+    oldKarma = Number(await fs.readFileSync(karmaHnFilePath, { encoding: 'ascii' }));
+  } catch (error) {
+    // Ignore the failed attempt to read the old karma if the file doesn't exist yet
+  }
 
-    response.setEncoding('ascii');
-    let content = '';
-    response.on('data', chunk => content += chunk);
-    response.on('end', async () => {
-      const regex = token ? userPageRegex : homePageRegex;
-      const match = regex.exec(content);
-      if (!match) {
-        throw new Error(`The pattern ${regex.source} not found on ${JSON.stringify(options)}.`);
-      }
+  await fs.writeFile(karmaHnFilePath, karma);
 
-      const karma = Number(match.groups.karma);
-      console.log(karma);
-      let oldKarma;
-      try {
-        oldKarma = Number(await fs.readFileSync(path.join(__dirname, 'karma.hn'), { encoding: 'ascii' }));
-      } catch (error) {
-        // Ignore the failed attempt to read the old karma if the file doesn't exist yet
-      }
+  if (oldKarma) {
+    const change = oldKarma !== karma
+      ? `It's ${Math.abs(karma - oldKarma)} ${Math.sign(karma - oldKarma) === 1 ? 'up' : 'down'}`
+      : 'It has seen no change'
+      ;
 
-      await fs.writeFile(path.join(__dirname, 'karma.hn'), karma, () => void 0);
+    return `Hacker News karma is ${karma}. ${change} since last time.`;
+  }
 
-      const change = oldKarma && oldKarma !== karma
-        ? `It's ${Math.abs(karma - oldKarma)} ${Math.sign(karma - oldKarma) === 1 ? 'up' : 'down'}`
-        : 'It has seen no change'
-        ;
-
-      await email(
-        eml(
-          subject(`HN Karma: ` + karma),
-          sender('Hacker News <bot@hubelbauer.net>'),
-          recipient('Tomas Hubelbauer <tomas@hubelbaur.net>'),
-          `Your HN karma is ${karma}. ${change} since yesterday.`
-        )
-      );
-    });
-  });
-
-  return new Promise(resolve => request.end(resolve));
+  return `Hacker News karma is ${karma}.`;
 }
 
 if (process.cwd() === __dirname) {
-  void async function () {
-    await module.exports();
-  }()
+  module.exports(process.argv[2]);
 }
